7ce2beebaef249438eccb99c1dad414b8c8015ac
pankaj kumar <Pankajams1234@gmail.com>
2024-09-16 21:22:49 +0530

    frontend added with routing

---
+++ frontend/src/components/Quotes.tsx
@@ -0,0 +1,16 @@
+const Quotes = () => {
+  return (
+    <div className="bg-slate-200  h-screen flex justify-center flex-col" >
+        <div className="flex justify-center">
+        <div className="max-w-lg text-center text-3xl font-semibold ">
+        "Everyone has a voice, and every voice deserves to be heard. Share your ideas and inspire."
+        </div>
+        </div>
+        <div className="max-wd-md text-center flex flex-start text-xl font-base text-gray-600   ml-40 mt-10">
+        Ev Williams
+        </div>
+    </div>
+  )
+}
+
+export default Quotes
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,15 @@
+import Quotes from "../components/Quotes"
+
+const Signup = () => {
+  return (
+    <div>
+    <div className="grid grid-cols-2">
+        <div>Hey</div>
+        <Quotes/>
+    </div>
+        
+    </div>
+  )
+}
+
+export default Signup
