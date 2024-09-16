a8f1b7f12af6f4e36a096ab3ca2ff2fe042617b3
pankaj kumar <Pankajams1234@gmail.com>
2024-09-16 23:45:34 +0530

    test purpose

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
@@ -8,12 +8,27 @@ function App() {
   return (
     <>
     <BrowserRouter>
+    <nav>
+          {/* Navigation Links */}
+          <ul>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/Blog/1">Blog Post 1</Link> {/* Example Blog Post Link */}
+            </li>
+          </ul>
+        </nav>
     <Routes>
     <Route  path='/signup' element={<Signup/>}  />
     <Route path='/signin' element={<Signin/>} />
     <Route path='/Blog/:id' element={<Blog/>} />
     </Routes>
     </BrowserRouter>
+  
     </>
   )
 }
