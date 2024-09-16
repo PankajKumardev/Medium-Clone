c2c84d409781251959ff47c22fd10077bd2adae8
pankaj kumar <Pankajams1234@gmail.com>
2024-09-16 23:47:14 +0530

    backToOrg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,20 +8,6 @@ function App() {
   return (
     <>
     <BrowserRouter>
-    <nav>
-          {/* Navigation Links */}
-          <ul>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/signin">Sign In</Link>
-            </li>
-            <li>
-              <Link to="/Blog/1">Blog Post 1</Link> {/* Example Blog Post Link */}
-            </li>
-          </ul>
-        </nav>
     <Routes>
     <Route  path='/signup' element={<Signup/>}  />
     <Route path='/signin' element={<Signin/>} />
