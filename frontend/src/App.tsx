b74a8e483607c47fbb1a7236ef462abe964589e7
pankaj kumar <Pankajams1234@gmail.com>
2024-09-16 23:50:39 +0530

    /

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
